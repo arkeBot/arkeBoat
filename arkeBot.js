900d32f82b365facaed9bc1dbd46baf679669777
arkeBot <dragonator290@hotmail.co.uk>
2014-11-04 21:26:34 +0800

    Update arkeBot.js
    this should fix it up finally

--- arkeBot.js
+++ arkeBot.js
@@ -40,7 +40,7 @@
     var loadChat = function (cb) {
         if (!cb) cb = function () {
         };
-        $.get("https://rawgit.com/Yemasthui/basicBot/master/lang/langIndex.json", function (json) {
+        $.get("https://rawgit.com/arkeBot/arkeboat/master/lang/en.json", function (json) {
             var link = basicBot.chatLink;
             if (json !== null && typeof json !== "undefined") {
                 langIndex = json;
